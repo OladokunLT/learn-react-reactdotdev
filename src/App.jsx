4d0f9c63327577e9c8b5a38a5931afaa8f6881da
oladokunlt <lukmanoladokun86@gmail.com>
2023-08-20 01:58:38 +0100

    Conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function MyButton() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <button> I&apos;m a button </button>
+  )
+} 
+
+const user = {
+  name: "Lukman Oladokun",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+}
+
+function ConditionalRendering() {
+  let content, isLoggedIn = true;
+  if (!isLoggedIn) {
+    content = "You are logged in"
+  } else {
+    content = "Sign up first"
+  }
+  return(
+    <div> {content} </div>
+  )
+}
+
+
+function ShoppingList() {
+  const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+  ];
+
+  const listItems = products.map(product => <li key={product.id}
+    style={{ color: product.isFruit ? "Orange" : "Brown" }}
+  > {product.title} </li>
+  );
+  return (<ul> {listItems} </ul>);
+}
+
+function App() {
+  return (
+    <div>
+      {/* Creating and nesting Components */}
+      <h1>Welcome to my app</h1>
+      <MyButton />
+      {/* Creating and nesting component */}
+
+      {/* Writing markup with JSX */}
+      <h2>{user.name}</h2>
+      <img 
+        src={user.imageUrl} 
+        alt={'Photo of ' + user.name} 
+        style = {{
+          width: user.imageSize,
+          height: user.imageSize,
+          borderRadius: "50%",
+        }}
+      />
+      <ConditionalRendering />
+      <ShoppingList />
+    </div>
   )
 }
 
